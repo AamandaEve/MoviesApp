42407283f6dbaf5007f8133defa7bc7f780cad1b
Amanda Evellin <amandasousav19@gmail.com>
2025-07-03 22:04:50 -0300

    refatorando categories

--- src/App.js
+++ src/App.js
@@ -3,20 +3,7 @@ import Footer from "./components/Footer";
 import Banner from "./components/Banner";
 import Container from "./components/Container";
 import Card from "./components/Card";
-import videos from "./json/videos.json";
-import Category from "./components/Category";
-
-const categories = [
-  "Geografia",
-  "Como fazer e usar",
-  "Astronomia e Geografia",
-  "Climatologia, Metereologia, Vegetação",
-  "Geologia e Hidrografia",
-];
-
-function filterCategory(id) {
-  return videos.filter((video) => video.category === categories[id]);
-}
+import Category, { categories, filterCategory } from "./components/Category";
 
 function App() {
   return (
@@ -25,16 +12,13 @@ function App() {
       <Banner image="inicio" />
 
       <Container>
-
-
-        { categories.map((category, index) => 
+        {categories.map((category, index) => (
           <Category category={category}>
-            { filterCategory(index).map((video) => <Card id={video.id} key=
-            {video.id} /> )}
+            {filterCategory(index).map((video) => (
+              <Card id={video.id} key={video.id} />
+            ))}
           </Category>
-        )}
-      
-      
+        ))}
       </Container>
 
       <Footer />
