615ad7b267339989179b1ba38f6ec4ab9aff7856
Amanda Evellin <amandasousav19@gmail.com>
2025-07-03 22:56:39 -0300

    Criação pagina watch

--- src/pages/Watch/index.js
+++ src/pages/Watch/index.js
@@ -1,11 +1,20 @@
+import Header from "../../components/Header";
+import Container from "../../components/Container";
+import Footer from "../../components/Footer";
 import styles from "./Watch.module.css";
 
-function Watch(){
-    return(
+function Watch() {
+  return (
+    <>
+      <Header />
+      <Container>
         <section className={styles.watch}>
-            <h1>Assistir</h1>
+          <iframe width="854" height="480" src="https://www.youtube.com/embed/vSkBlGZtxr0?si=tKWpGB7do9yf6vo8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </section>
-    );
+      </Container>
+      <Footer />
+    </>
+  );
 }
 
 export default Watch;
