64c81c3a3d50b6b40ffaa74ddc79e45e6302b674
Amanda Evellin <amandasousav19@gmail.com>
2025-07-03 22:17:07 -0300

    ConfigHome e Watch

--- src/App.js
+++ src/App.js
@@ -1,29 +1,7 @@
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Banner from "./components/Banner";
-import Container from "./components/Container";
-import Card from "./components/Card";
-import Category, { categories, filterCategory } from "./components/Category";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Banner image="inicio" />
-
-      <Container>
-        {categories.map((category, index) => (
-          <Category category={category}>
-            {filterCategory(index).map((video) => (
-              <Card id={video.id} key={video.id} />
-            ))}
-          </Category>
-        ))}
-      </Container>
-
-      <Footer />
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
