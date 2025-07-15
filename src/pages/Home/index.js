7519ea8b4282abbe343933851db50320aa185776
Amanda Evellin <amandasousav19@gmail.com>
2025-07-15 15:46:27 -0300

    Hooks e useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,21 +4,47 @@ import Banner from "../../components/Banner";
 import Container from "../../components/Container";
 import Card from "../../components/Card";
 import Category, { categories, filterCategory } from "../../components/Category";
+import { useState } from "react";
 
 function Home() {
+  
+  // let nomeDigitado = '';
+
+  function pegarNome(event ){
+    console.log(event.target.value) /*pegar valor digitado*/
+    // nomeDigitado = event.target.value;
+    setNome(event.target.value)
+  }
+
+  //Hook = gancho -> useState
+  const [nome, setNome] = useState();
+
+  
   return (
     <div>
       <Header />
       <Banner image="inicio" />
 
       <Container>
-        {categories.map((category, index) => (
+
+        <input
+          type="text"
+          placeholder="Nome"
+
+          onChange={pegarNome}
+          />
+          <h2>{nome}</h2>
+
+
+
+
+        {/* {categories.map((category, index) => (
           <Category category={category}>
             {filterCategory(index).map((video) => (
               <Card id={video.id} key={video.id} />
             ))}
           </Category>
-        ))}
+        ))} */}
       </Container>
 
       <Footer />
