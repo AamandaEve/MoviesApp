2498fb59d63d42064ebe8b18ad279698772d59eb
Amanda Evellin <amandasousav19@gmail.com>
2025-07-15 15:01:33 -0300

    Rota dinamica

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,18 +1,18 @@
+import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
 
 /*link e imagem */
-function Card({id}){
-    return(
-        <section className={styles.card}>
-            <a
-                href={`https://youtu.be/${id}`}
-                rel="noopener noreferrer"
-                target="_blank">
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`} 
-                alt="Capa"></img>
-            </a>
-        </section>
-    );
+function Card({ id }) {
+  return (
+    <section className={styles.card}>
+      <Link to={`/watch/${id}`}>
+        <img
+          src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
+          alt="Capa"
+        ></img>
+      </Link>
+    </section>
+  );
 }
 
 export default Card;
