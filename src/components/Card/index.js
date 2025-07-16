9069daf48e686c0aef2e8ee70da423973e040dec
Amanda Evellin <amandasousav19@gmail.com>
2025-07-16 20:45:30 -0300

    Css e adição de favoritos

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import iconFavorite from "./favorite.png";
+import iconUnfavorite from "./unfavorite.png";
 
 /*link e imagem */
 function Card({ id }) {
@@ -8,9 +10,13 @@ function Card({ id }) {
       <Link to={`/watch/${id}`}>
         <img
           src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}
-          alt="Capa"
+          alt="Capa" className="styles.capa"
         ></img>
       </Link>
+
+      <figure className={styles.icon}>
+        <img src={iconFavorite} alt="icone" />
+      </figure>
     </section>
   );
 }
--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,10 +1,22 @@
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import Container from "../../components/Container";
 import styles from "./Favorites.module.css";
 
 function Favorites(){
     return (
-        <section>
+        <>
+        <Header/>
+        <Container>
+            <section className={styles.favorites}>
+            <h2>Meus favoritos</h2>
+                Lista de favoritos
+            </section>
 
-        </section>
+
+        </Container>
+        <Footer/>
+        </>
     )
 }
 
